cf7a9ec89ee75addf9b593027be5aaba9fe0561f
Mominul Haque <sejan840@gmail.com>
2023-07-23 00:34:26 +0600

    OK

--- src/components/common/svg/RightArrow.jsx
+++ src/components/common/svg/RightArrow.jsx
@@ -1,10 +1,12 @@
-const RightArrow = ({strokeWidth=2, currentColor="currentColor"}) => {
+const RightArrow = ({ strokeWidth = 1.5, currentColor = "currentColor" }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke={currentColor} className="w-10 h-6">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-  </svg>
-  
-  
+    //   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke={currentColor} className="w-10 h-6">
+    //   <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+    // </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  strokeWidth={strokeWidth} stroke={currentColor} className="w-10 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+    </svg>
+
   )
 }
 
