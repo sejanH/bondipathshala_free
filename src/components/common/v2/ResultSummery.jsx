f90720ccce96f20db1c37f7187b3b4046399783e
Mominul Haque <sejan840@gmail.com>
2023-07-24 01:21:32 +0600

    ok partially

--- src/components/common/v2/ResultSummery.jsx
+++ src/components/common/v2/ResultSummery.jsx
@@ -16,7 +16,7 @@ function ResultSummery({ title = 'Exam Result', result = {}, bgColor = 'warning'
                     <h3 className="text-3xl font-bold py-1 text-center text-white bg-title-2">{title}</h3>
                     {!hideCloseBtn && (<label htmlFor="my-modal-4" className="btn bg-inactive border-0 btn-xs btn-circle absolute right-2 top-2"><img className="w-3 h-3" src={cross} /></label>)}
                     <div className="relative md:min-h-[18rem] overflow-auto p-4">
-                        <h2 className="text-3xl font-bold mb-2">{result.examName} ({result.examVariation} Exam)</h2>
+                        <h2 className="text-3xl font-bold mb-2">{result.examName} (Free Exam)</h2>
                         <div className="grid tab:grid-cols-5 grid-flow-rows gap-x-4 tab-max:grid-cols-2 sm-min:grid-cols-2 gap-y-4 place-content-center resultSummery">
                             <div className="resultSummery_col">
                                 <span className="resultSummery_col_top">Total Questions</span>
