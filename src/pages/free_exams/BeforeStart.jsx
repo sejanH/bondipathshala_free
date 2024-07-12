70e3972d8250d8655ec248876bce86f437a4a81e
unknown <uiu.afser@gmail.com>
2024-07-13 02:19:15 +0600

    curriculum column added

--- src/pages/free_exams/BeforeStart.jsx
+++ src/pages/free_exams/BeforeStart.jsx
@@ -251,6 +251,29 @@ const BeforeStart = () => {
                       </>
                     )
                   }
+                    {examDetails.curriculumName !== null &&
+                      examDetails.curriculumName !== 'null' && (
+                        <>
+                          <div className="form-control mb-4">
+                            <label className="label font-bold relative">
+                              <span className="absolute top-1 left-3 bg-white px-2 text-title-2">
+                                {examDetails.curriculumName + ' '}
+                                {examDetails.isAdmission === true &&
+                                  'Admission '}{' '}
+                                Roll no
+                              </span>
+                            </label>
+                            <input
+                              type="text"
+                              required
+                              name="sscRoll"
+                              placeholder="Roll no"
+                              className="input border-2 border-title-2 focus:border-orange-600"
+                            />
+                          </div>
+                        </>
+                      )}
+                      
                   <div className="form-control mb-4">
                     <label className="label font-bold relative">
                       <span className="absolute top-1 left-3 bg-white px-2 text-title-2">Institution Name</span>
