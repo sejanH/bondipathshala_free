3aa9246d9873c616a7ed814264771c3689ca192d
unknown <uiu.afser@gmail.com>
2024-10-14 07:20:02 +0600

    text question option position design updated

--- src/pages/free_exams/OngoingExam.jsx
+++ src/pages/free_exams/OngoingExam.jsx
@@ -38,6 +38,7 @@ const OngoingExam = () => {
   useEffect(() => {
     if (TOKEN) {
       // window.scrollTo(0,0)
+      window.location.hash = "#countDownDiv";
       axios.defaults.headers.common['Authorization'] = 'Bearer ' + TOKEN;
 
       axios.get('/api/freestudent/examcheckmiddleware?eId=' + params.get('examId'))
@@ -160,7 +161,7 @@ const OngoingExam = () => {
     <div className="px-28 md:px-4">
       <div className="pb-8 container mx-auto max-w-4xl">
         {/* examInoDetails */}
-        <div className="bg-white rounded-xl" tabIndex={0}>
+        <div className="bg-white rounded-xl" tabIndex={0} id="countDownDiv">
           {examData && (<div className="flex btn-theme rounded-t-xl py-1 justify-center">
             <span className="text-2xl font-bold capitalize text-white">Free Live Exam</span>
           </div>)}
