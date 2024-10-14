4cf844a06934a8d1403787fddfca0d124e620ec8
unknown <uiu.afser@gmail.com>
2024-10-14 07:08:39 +0600

    text question option position design updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { useEffect } from "react";
-import { Route, Routes,useLocation } from "react-router-dom";
+import { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 // import authChecker from "./utils/authChecker";
-import { pages } from "./routes";
+import { pages } from './routes'
+import ScrollToTop from './hook/ScrollToTop'
 // authChecker();
 const App = () => {
-  const { pathname } = useLocation();
+  const { pathname } = useLocation()
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    window.scrollTo(0, 0)
+  }, [pathname])
   return (
     <div>
       <Routes>
-        {pages.map((page, index) => (
-          <Route key={index} element={<page.element />}>
-            {page.children.map((pg, indx) => (
-              <Route key={indx} path={pg.path} element={<pg.element />} />
-            ))}
-          </Route>
-        ))}
-
+        <ScrollToTop>
+          {pages.map((page, index) => (
+            <Route key={index} element={<page.element />}>
+              {page.children.map((pg, indx) => (
+                <Route key={indx} path={pg.path} element={<pg.element />} />
+              ))}
+            </Route>
+          ))}
+        </ScrollToTop>
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
